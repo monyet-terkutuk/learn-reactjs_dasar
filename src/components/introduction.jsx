50fd912ddfcddb0b9fdb1446878e4011c660b3e9
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-05-25 11:41:14 +0700

    - Belajar React dasar

---
+++ src/components/introduction.jsx
@@ -0,0 +1,10 @@
+function Introduction(props) {
+  return (
+    <div >
+          <h1>Introduction</h1>
+          <p>Nama saya adalah : { props.name }</p>
+    </div>
+  );
+}
+
+export default Introduction;
---
+++ src/components/profile.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Profile extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+        <div>
+            <h1>Latihan React</h1>
+      </div>
+    )
+  }
+}
+
+export default Profile
